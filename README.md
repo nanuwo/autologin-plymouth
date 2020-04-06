feecd542c380e2147c7c7666237c917f88464a81
Kenny Levinsen <kl@kl.wtf>
2020-04-06 21:52:42 +0200

    Minor cleanup

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ It logs you in. Automatically.
 meson build
 ninja -C build
 ```
+
+## How to use
+
+1. Create `/etc/pam.d/autologin`. You can use `/etc/pam.d/login` as source.
+2. Install binary to /usr/local/bin/, systemd unit to /etc/systemd/system/
+3. Change username and start command in systemd unit
+3. Enable and start.
+
+(Note, systemd is not required)
--- main.c
+++ main.c
@@ -16,14 +16,14 @@
 #include <linux/vt.h>
 #include <security/pam_appl.h>
 
-int handle_error(char *operation, int error) {
+int check_errno(char *operation, int error) {
 	if (error != -1)
 		return error;
 	fprintf(stderr, "%s failed: %s (%d)\n", operation, strerror(errno), errno);
 	exit(EXIT_FAILURE);
 }
 
-int handle_pam_error(struct pam_handle *handle, char *operation, int error) {
+int check_pam(struct pam_handle *handle, char *operation, int error) {
 	if (error == PAM_SUCCESS)
 		return 0;
 	fprintf(stderr, "%s failed: %s\n", operation, pam_strerror(handle, error));
@@ -32,7 +32,7 @@ int handle_pam_error(struct pam_handle *handle, char *operation, int error) {
 	exit(EXIT_FAILURE);
 }
 
-int null_handle(int num_msg, const struct pam_message** msg, struct pam_response** resp, void* appdata_ptr) {
+int pam_null_conv(int num_msg, const struct pam_message** msg, struct pam_response** resp, void* appdata_ptr) {
 	*resp = calloc(num_msg, sizeof (struct pam_response));
 	return *resp == NULL ? PAM_BUF_ERR : PAM_SUCCESS;
 }
@@ -43,12 +43,30 @@ int pam_putenv_tuple(struct pam_handle* handle, char* key, char* value) {
 	return pam_putenv(handle, buf);
 }
 
-void child(struct passwd *pwd, char* user_cmd, char** env) {
-	handle_error("initgroups", initgroups(pwd->pw_name, pwd->pw_gid));
-	handle_error("setgid", setgid(pwd->pw_gid));
-	handle_error("setuid", setuid(pwd->pw_uid));
-	handle_error("chdir", chdir(pwd->pw_dir));
-	handle_error("prctl", prctl(PR_SET_PDEATHSIG, SIGTERM));
+void setup_vt(int vt) {
+	char p[32];
+	snprintf(p, sizeof p, "/dev/tty%d", vt);
+	int fd = check_errno("open tty", open(p, O_RDWR));
+
+	check_errno("dup2", dup2(fd, 0));
+	check_errno("dup2", dup2(fd, 1));
+	check_errno("dup2", dup2(fd, 2));
+
+	struct vt_mode mode = { 0 };
+	mode.mode = VT_AUTO;
+	check_errno("KDSETMODE", ioctl(fd, KDSETMODE, KD_TEXT));
+	check_errno("VT_SETMODE", ioctl(fd, VT_SETMODE, &mode));
+	check_errno("VT_ACTIVATE", ioctl(fd, VT_ACTIVATE, vt));
+	check_errno("VT_WAITACTIVE", ioctl(fd, VT_WAITACTIVE, vt));
+	close(fd);
+}
+
+void child_main(struct passwd *pwd, char* user_cmd, char** env) {
+	check_errno("initgroups", initgroups(pwd->pw_name, pwd->pw_gid));
+	check_errno("setgid", setgid(pwd->pw_gid));
+	check_errno("setuid", setuid(pwd->pw_uid));
+	check_errno("chdir", chdir(pwd->pw_dir));
+	check_errno("prctl", prctl(PR_SET_PDEATHSIG, SIGTERM));
 
 	char arg[1024];
 	snprintf(arg, sizeof arg, "[ -f /etc/profile ] && . /etc/profile; [ -f $HOME/.profile ] && . $HOME/.profile; exec %s", user_cmd);
@@ -56,33 +74,14 @@ void child(struct passwd *pwd, char* user_cmd, char** env) {
 	execvpe(cmd[0], cmd, env);
 }
 
-void setup_vt(int vt) {
-	char p[32];
-	snprintf(p, sizeof p, "/dev/tty%d", vt);
-	int fd = handle_error("open tty", open(p, O_RDWR));
-
-	handle_error("dup2", dup2(fd, 0));
-	handle_error("dup2", dup2(fd, 1));
-	handle_error("dup2", dup2(fd, 2));
-
-	struct vt_mode mode = { 0 };
-	mode.mode = VT_AUTO;
-	handle_error("KDSETMODE", ioctl(fd, KDSETMODE, KD_TEXT));
-	handle_error("VT_SETMODE", ioctl(fd, VT_SETMODE, &mode));
-	handle_error("VT_ACTIVATE", ioctl(fd, VT_ACTIVATE, vt));
-	handle_error("VT_WAITACTIVE", ioctl(fd, VT_WAITACTIVE, vt));
-	close(fd);
-}
-
 int main(int argc, char *argv[]) {
-	struct pam_conv conv = { null_handle, NULL };
+	struct pam_conv conv = { pam_null_conv, NULL };
 	struct pam_handle* handle;
 
-	handle_pam_error(handle, "pam_start", pam_start("greetd", argv[1], &conv, &handle));
-	handle_pam_error(handle, "pam_acct_mgmt", pam_acct_mgmt(handle, PAM_SILENT));
-	handle_pam_error(handle, "pam_setcred", pam_setcred(handle, PAM_ESTABLISH_CRED));
+	check_pam(handle, "pam_start", pam_start("autologin", argv[1], &conv, &handle));
+	check_pam(handle, "pam_acct_mgmt", pam_acct_mgmt(handle, PAM_SILENT));
+	check_pam(handle, "pam_setcred", pam_setcred(handle, PAM_ESTABLISH_CRED));
 
-//	handle_error("setsid", setsid());
 	setup_vt(1);
 
 	struct passwd* pwd = getpwnam(argv[1]);
@@ -91,22 +90,22 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	handle_pam_error(handle, "pam_putenv", pam_putenv(handle, "XDG_SEAT=seat0"));
-	handle_pam_error(handle, "pam_putenv", pam_putenv(handle, "XDG_SESSION_CLASS=user"));
-	handle_pam_error(handle, "pam_putenv", pam_putenv(handle, "XDG_VTNR=1"));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "HOME", pwd->pw_dir));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "PWD", pwd->pw_dir));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "SHELL", pwd->pw_shell));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "USER", pwd->pw_name));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "LOGNAME", pwd->pw_name));
-	handle_pam_error(handle, "pam_putenv", pam_putenv_tuple(handle, "TERM", getenv("TERM") ?: "linux"));
+	check_pam(handle, "pam_putenv", pam_putenv(handle, "XDG_SEAT=seat0"));
+	check_pam(handle, "pam_putenv", pam_putenv(handle, "XDG_SESSION_CLASS=user"));
+	check_pam(handle, "pam_putenv", pam_putenv(handle, "XDG_VTNR=1"));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "HOME", pwd->pw_dir));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "PWD", pwd->pw_dir));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "SHELL", pwd->pw_shell));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "USER", pwd->pw_name));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "LOGNAME", pwd->pw_name));
+	check_pam(handle, "pam_putenv", pam_putenv_tuple(handle, "TERM", getenv("TERM") ?: "linux"));
 
-	handle_pam_error(handle, "pam_open_session", pam_open_session(handle, PAM_SILENT));
+	check_pam(handle, "pam_open_session", pam_open_session(handle, PAM_SILENT));
 	char** env = pam_getenvlist(handle);
 
-	pid_t pid = handle_error("fork", fork());
+	pid_t pid = check_errno("fork", fork());
 	if (pid == 0)
-		child(pwd, argv[2], env);
+		child_main(pwd, argv[2], env);
 
 	int res;
 	while ((res = waitpid(pid, NULL, 0)) <= 0) {
@@ -116,8 +115,8 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	handle_pam_error(handle, "pam_close_session", pam_close_session(handle, PAM_SILENT));
-	handle_pam_error(handle, "pam_setcred", pam_setcred(handle, PAM_DELETE_CRED));
-	handle_pam_error(handle, "pam_end", pam_end(handle, 0));
+	check_pam(handle, "pam_close_session", pam_close_session(handle, PAM_SILENT));
+	check_pam(handle, "pam_setcred", pam_setcred(handle, PAM_DELETE_CRED));
+	check_pam(handle, "pam_end", pam_end(handle, 0));
 	return 0;
 }
